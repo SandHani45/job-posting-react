c396731e996f63a7446920debcd57f7e4961b304
Shaik <sandhani.shaik@pearson.com>
2020-04-06 01:23:08 +0530

    api che

--- src/service/employee.js
+++ src/service/employee.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
-const endPoint = "https://dog.ceo/api"
+const endPoint = "http://34.206.72.199:3001/api/pc"
 export const getWorkCellService = () => {
-    return axios.get(`${endPoint}/breeds/image/random/`).then((res) => res.data);
+    return axios.get(`/workcell/`).then((res) => res.data);
 };
 export const getPanelShopService = (id) => {
-    return axios.get(`${endPoint}/employee/${id}`).then((res) => res.data);
+    return axios.get(`$/employee/${id}`).then((res) => res.data);
 };
 export const getWorkOrderService = (id) => {
-    return axios.get(`${endPoint}/checkWorkOrder/${id}`).then((res) => res.data);
+    return axios.get(`$/checkWorkOrder/${id}`).then((res) => res.data);
 };
 export const getLaborActivityService = (key,id) => {
-    return axios.get(`${endPoint}/laborActivity/${key}/${id}`).then((res) => res.data);
+    return axios.get(`$/laborActivity/${key}/${id}`).then((res) => res.data);
 };
 // export const getLaborConfirmService = ({deptKey, woNo, plantKey, wcellKey, wcenterKey, employee}) => {
 //     return axios.get(`${endPoint}/laborConfirm/${deptKey}/${woNo}/${plantKey}/${wcellKey}/${wcenterKey}/${employee}`).then((res) => res.data);
 // };
 // demo 
 export const getLaborConfirmService = ({deptKey, woNo, plantKey, wcellKey, wcenterKey, employee}) => {
-    return axios.get(`${endPoint}/laborConfirm/31/3443151/97/2/157/55`).then((res) => res.data);
+    return axios.get(`$/laborConfirm/31/3443151/97/2/157/55`).then((res) => res.data);
 };
