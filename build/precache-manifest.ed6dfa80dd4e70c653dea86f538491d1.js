d7a7e741bc6527ab2322b806ea8e9d324c81a371
Shaik <sandhani.sk45@gmail.com>
2020-05-02 20:34:35 +0530

    new build

---
+++ build/precache-manifest.ed6dfa80dd4e70c653dea86f538491d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f3d81a988993b177a58efdd267f15d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "543069f48f56bab47bdd",
+    "url": "./static/css/2.96f95cc7.chunk.css"
+  },
+  {
+    "revision": "8ed4435c462c6c6c3709",
+    "url": "./static/css/main.a04eb1da.chunk.css"
+  },
+  {
+    "revision": "543069f48f56bab47bdd",
+    "url": "./static/js/2.e4eee2df.chunk.js"
+  },
+  {
+    "revision": "96219bc210d065416bcec57f9037d9d9",
+    "url": "./static/js/2.e4eee2df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed4435c462c6c6c3709",
+    "url": "./static/js/main.257ac533.chunk.js"
+  },
+  {
+    "revision": "653d7a619c4800e50f20",
+    "url": "./static/js/runtime-main.0a5f132a.js"
+  }
+]);
