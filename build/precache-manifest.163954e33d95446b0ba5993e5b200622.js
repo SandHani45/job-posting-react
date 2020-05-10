84b5c10d9c3c9e302a65855aa3132e0f66826ee0
Shaik <sandhani.sk45@gmail.com>
2020-05-10 21:11:56 +0530

    bufff

---
+++ build/precache-manifest.163954e33d95446b0ba5993e5b200622.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cfd8330846bcdf021f51815ae2182e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7e106dcde3612c6f95c",
+    "url": "./static/css/2.96f95cc7.chunk.css"
+  },
+  {
+    "revision": "31a59cde1e9a0568988b",
+    "url": "./static/css/main.7e6add80.chunk.css"
+  },
+  {
+    "revision": "d7e106dcde3612c6f95c",
+    "url": "./static/js/2.66ce4974.chunk.js"
+  },
+  {
+    "revision": "7e3464e69bc724e33e95432ed3bff543",
+    "url": "./static/js/2.66ce4974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a59cde1e9a0568988b",
+    "url": "./static/js/main.ca82d47f.chunk.js"
+  },
+  {
+    "revision": "653d7a619c4800e50f20",
+    "url": "./static/js/runtime-main.0a5f132a.js"
+  }
+]);
